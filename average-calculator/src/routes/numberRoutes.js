3f3207ecd4c70791282a7abe00d47718fd77070a
SushAN766 <sushanthbs766@gmail.com>
2025-05-10 15:41:51 +0530

    Updated routes and added contollers

--- average-calculator/src/routes/numberRoutes.js
+++ average-calculator/src/routes/numberRoutes.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { fetchNumbersFromAPI, updateWindow } = require('..src/services/numberService');
 
-router.get('/', async (req, res) => {
-  try {
-    const numbers = await fetchNumbersFromAPI();
-    const updated = updateWindow(numbers);
-    res.json({ numbers: updated });
-  } catch (err) {
-    res.status(408).json({ error: 'API Timeout or Error' });
-  }
-});
+const { handleNumberRequest } = require('../controllers/numberController');
 
+router.get('/:numberid', handleNumberRequest);
 module.exports = router;
+
+
+
+
