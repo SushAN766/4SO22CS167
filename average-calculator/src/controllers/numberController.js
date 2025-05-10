f28c49d20ada1ace4955b51e81ec2ef491960836
SushAN766 <sushanthbs766@gmail.com>
2025-05-10 16:16:39 +0530

    modified:server,controllers,services

--- average-calculator/src/controllers/numberController.js
+++ average-calculator/src/controllers/numberController.js
@@ -1,24 +1,21 @@
 const { fetchNumbers, updateWindow, getWindow, getAvg } = require('../services/numberService');
 const { VALID_IDS } = require('../utils/constants');
-
 exports.handleNumberRequest = async (req, res) => {
-  const id = req.params.numberid;
-
-  if (!VALID_IDS[id]) {
-    return res.status(400).json({ error: 'Invalid number ID' });
-  }
-
-  const prevWindow = getWindow();
-  const numbers = await fetchNumbers(VALID_IDS[id]);
-  updateWindow(numbers);
-  const currWindow = getWindow();
-  const avg = getAvg();
-
-  res.json({
-    windowPrevState: prevWindow,
-    windowCurrState: currWindow,
-    numbers,
-    avg
-  });
-};
-
+    const id = req.params.numberid;
+    const { VALID_IDS } = require('../utils/constants');
+    if (!VALID_IDS[id]) {
+      return res.status(400).json({ error: 'Invalid ID' });
+    }
+    const prevWindow = [...window];
+    const numbers = await fetchNumbers(VALID_IDS[id]);
+    updateWindow(numbers);
+    const currWindow = [...window];
+    const avg = window.length ? parseFloat((window.reduce((a, b) => a + b, 0) / window.length).toFixed(2)) : 0;
+    res.json({
+      windowPrevState: prevWindow,
+      windowCurrState: currWindow,
+      numbers,
+      avg
+    });
+  };
+  
